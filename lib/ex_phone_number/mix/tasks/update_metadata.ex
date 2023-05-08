00af10b8b07b8baf7cf16e2e7d8edd13734330fb
Kamil Kowalski <kamil@fresha.com>
2023-05-08 21:48:09 +0200

    Improve the update_metadata task and update to v8.13.11

--- lib/ex_phone_number/mix/tasks/update_metadata.ex
+++ lib/ex_phone_number/mix/tasks/update_metadata.ex
@@ -1,58 +1,55 @@
-if Code.ensure_loaded?(Tesla) do
-  defmodule Mix.Tasks.UpdateMetadata do
-    @moduledoc "Downloads the latest libphonenumber metadata from GitHub"
-    @shortdoc "Update libphonenumber metadata"
+defmodule Mix.Tasks.UpdateMetadata do
+  @moduledoc "Downloads the latest libphonenumber metadata from GitHub"
+  @shortdoc "Update libphonenumber metadata"
 
-    use Mix.Task
-    @raw_files_url "https://raw.githubusercontent.com/google/libphonenumber"
-    @files_to_download ["resources/PhoneNumberMetadata.xml", "resources/PhoneNumberMetadataForTesting.xml"]
-    @resources_directory "resources"
+  use Mix.Task
+  require Logger
 
-    defmodule GitHub do
-      def latest_release(repo) do
-        Tesla.get(client(), "/repos/" <> repo <> "/releases/latest")
-      end
+  @raw_files_url "https://raw.githubusercontent.com/google/libphonenumber"
+  @files_to_download [
+    "resources/PhoneNumberMetadata.xml",
+    "resources/PhoneNumberMetadataForTesting.xml"
+  ]
+  @resources_directory "resources"
 
-      defp client do
-        middleware = [
-          {Tesla.Middleware.BaseUrl, "https://api.github.com"},
-          Tesla.Middleware.JSON,
-          {Tesla.Middleware.Headers, [{"User-Agent", "ex_phone_number"}]}
-        ]
+  @impl Mix.Task
+  def run(_args) do
+    Application.ensure_all_started(:req)
+    latest_tag = fetch_latest_tag()
+    Logger.info("Downloading metadata #{latest_tag}")
+    Enum.each(@files_to_download, &download(latest_tag, &1))
+    update_readme(latest_tag)
+  end
 
-        Tesla.client(middleware)
-      end
-    end
+  defp fetch_latest_tag() do
+    Req.get!(req(), url: "https://api.github.com/repos/google/libphonenumber/releases/latest").body[
+      "tag_name"
+    ]
+  end
 
-    @impl Mix.Task
-    def run(_args) do
-      latest_tag = fetch_latest_tag()
-      Enum.each(@files_to_download, &download(latest_tag, &1))
-      update_readme(latest_tag)
-    end
+  defp download(tag, path) do
+    filename = Path.basename(path)
+    local_path = Path.join([File.cwd!(), @resources_directory, filename])
+    file_url = "#{@raw_files_url}/#{tag}/#{path}"
+    %{status: 200, body: body} = Req.get!(req(), url: file_url)
+    File.write!(local_path, body)
+  end
 
-    defp fetch_latest_tag() do
-      {:ok, %{body: body}} = GitHub.latest_release("google/libphonenumber")
-      body["tag_name"]
-    end
+  defp update_readme(tag) do
+    readme_path = Path.join([File.cwd!(), "README.md"])
 
-    defp download(tag, path) do
-      filename = Path.basename(path)
-      local_path = Path.join([File.cwd!(), @resources_directory, filename])
-      file_url = "#{@raw_files_url}/#{tag}/#{path}"
-      {:ok, %{body: body}} = Tesla.get(file_url)
-      File.write!(local_path, body)
-    end
+    updated_content =
+      readme_path
+      |> File.read!()
+      |> String.replace(
+        ~r{Current metadata version: v\d+\.\d+\.\d+\.},
+        "Current metadata version: " <> tag <> "."
+      )
 
-    defp update_readme(tag) do
-      readme_path = Path.join([File.cwd!(), "README.md"])
+    File.write!(readme_path, updated_content)
+  end
 
-      updated_content =
-        readme_path
-        |> File.read!()
-        |> String.replace(~r{Current metadata version: v[\d+].[\d+].[\d+]\.}, "Current metadata version: " <> tag <> ".")
-
-      File.write!(readme_path, updated_content)
-    end
+  defp req() do
+    Req.new(headers: [user_agent: "ex_phone_number"])
   end
 end
