f6603a57f4fe8e9cb85123b8ad8e73619bf1115b
Szymon Jeż <35540+szymon-jez@users.noreply.github.com>
2025-06-22 20:29:32 +0200

    Drop Elixir versions below 1.14, and patch the remaining ones
    Resolves #83

--- mix.exs
+++ mix.exs
@@ -9,7 +9,7 @@ defmodule ExPhoneNumber.Mixfile do
       app: :ex_phone_number,
       version: @version,
       name: "ExPhoneNumber",
-      elixir: "~> 1.11",
+      elixir: "~> 1.14",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
       deps: deps(),
